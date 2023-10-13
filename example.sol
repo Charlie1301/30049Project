09b6f183858da0e2211ddec6bf6c43e44568152b
Charlie1301 <CharlieJack_1301@outlook.com>
2023-10-14 08:53:02 +1100

    adding example.sol

---
+++ example.sol
@@ -0,0 +1,40 @@
+<<<<<<< HEAD
+pragma solidity ^0.8.1;
+
+contract FirstContract {
+
+    uint var1;
+
+    function set(uint x) public {
+
+        var1 = x;
+
+    }
+
+    function get() public view returns (uint) {
+
+        return var1;
+
+    }
+
+=======
+pragma solidity ^0.8.1;
+
+contract FirstContract {
+
+    uint var1;
+
+    function set(uint x) public {
+
+        var1 = x;
+
+    }
+
+    function get() public view returns (uint) {
+
+        return var1;
+
+    }
+
+>>>>>>> 5f744f0d6c738c19adda23b3b66543b21d2580d3
+}
